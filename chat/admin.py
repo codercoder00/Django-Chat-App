eeb0e215b040601b7408b8f6cb803723842f4404
VeyselKaraali <veysel-karaali@outlook.de>
2023-05-28 23:00:02 +0200

    Authentication

--- chat/admin.py
+++ chat/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Message
+from .models import Chat, Message
 
 class MessageAdmin(admin.ModelAdmin):
-  fields = ('text', 'created_at', 'author', 'receiver')
+  fields = ('chat', 'text', 'created_at', 'author', 'receiver')
   list_display = ('text', 'created_at', 'author', 'receiver')
   search_fields = ('text',)
 
 # Register your models here.
 admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from .models import Message, Chat
 # Create your views here.
 def index(request):
   if request.method == 'POST':
     print('Received data ' + request.POST['textmessage'])
-  return render(request, 'chat/index.html', {'username':'User1'})
+    myChat = Chat.objects.get(id=1)
+    Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+  chatMessages = Message.objects.filter(chat__id=1)
+  return render(request, 'chat/index.html', {'messages': chatMessages})
