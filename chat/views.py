b1698abc67e420db1dac0d547ea7892ad72ea354
VeyselKaraali <veysel-karaali@outlook.de>
2023-05-28 17:27:55 +0200

    Added Token

--- chat/views.py
+++ chat/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+  if request.method == 'POST':
+    print('Received data ' + request.POST['textmessage'])
   return render(request, 'chat/index.html', {'username':'User1'})
