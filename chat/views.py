e1816d47d51176087a5042dbfca53153cd0b90fa
VeyselKaraali <veysel-karaali@outlook.de>
2023-05-29 20:25:52 +0200

    json Serializer

--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,11 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from .models import Message, Chat
 from django.http.response import HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.core import serializers
 
 # Create your views here.
 @login_required(login_url='/login/')
@@ -11,7 +13,9 @@ def index(request):
   if request.method == 'POST':
     print('Received data ' + request.POST['textmessage'])
     myChat = Chat.objects.get(id=1)
-    Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+    new_message = Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+    serialized_message = serializers.serialize('json', [new_message])
+    return JsonResponse(serialized_message[1:-1], safe=False)
   chatMessages = Message.objects.filter(chat__id=1)
   return render(request, 'chat/index.html', {'messages': chatMessages})
 
@@ -26,6 +30,7 @@ def login_view(request):
       return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
   return render(request, 'auth/login.html', {'redirect': redirect})
 
+
 def register_view(request):
   redirect = request.GET.get('next')
   if request.method == 'POST':
@@ -35,4 +40,4 @@ def register_view(request):
     else:
       user = User.objects.create_user(username='john', email='jlennon@beatles.com', password='glass onion')
       return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
-  return render(request, 'auth/login.html', {'redirect': redirect})
+  return render(request, 'auth/register.html', {'redirect': redirect})
